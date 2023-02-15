b89686849f83469bdfa5cd16f1b5fe9dd5880506
Darvesh <hello@darve.sh>
2023-02-15 22:48:04 +0530

    feat: Add 'Obsolete form of'

--- helper.ts
+++ helper.ts
@@ -39,6 +39,7 @@ const outliers = [
 	"Alternative form of ",
 	"Alternative spelling of ",
 	"plural of ",
+	"Obsolete form of "
 ] as const;
 
 async function api(word: string, language: "en" = "en"): Promise<List[]> {
