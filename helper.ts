e7fa6a1bf386eebeb586d4eaaa96830efc91992a
Darvesh <hello@darve.sh>
2025-11-11 20:49:58 +0530

    Refactor definition escape logic in helper.ts
    Removed specific CSS class replacement in definition processing.

--- helper.ts
+++ helper.ts
@@ -31,7 +31,8 @@ function escape(text: string) {
     .join(".\n")
     .replaceAll("<", "&lt;")
     .replaceAll(">", "&gt;")
-    .replaceAll("&", "&amp;");
+    .replaceAll("&", "&amp;")
+    .replace(".mw-parser-output .defdate{font-size:smaller}", "")
 }
 
 const outliers = [
@@ -87,7 +88,7 @@ async function api(word: string): Promise<List[]> {
       word: escape(word),
       language: escape(dict.language),
       partOfSpeech: escape(dict.partOfSpeech),
-      definition: escape(def.definition.replace(".mw-parser-output .defdate{font-size:smaller}", "")),
+      definition: escape(def.definition),
       examples: def?.examples?.map((example) => escape(example)),
     }))
   );
